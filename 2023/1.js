656b21b928c1bfbbe4a6e1d8a728f8d05fefbe2b
nick.peterson <nick.peterson@target.com>
2023-12-01 17:46:40 -0600

    day 1

--- 2023/1.js
+++ 2023/1.js
@@ -1,39 +1,29 @@
 var argv = require('minimist')(process.argv.slice(2));
 
 const solve = (lines) => {
-  if (lines.length <= 0) {
-    return 0;
-  }
-
+  if (lines.length <= 0) return 0;
   return getValueOfLine(lines[0]) + solve(lines.slice(1));
 };
 
 const getValueOfLine = (line) => {
-  let result = 0;
+  const digits = getDigits(line);
+  return Number(digits[0]) * 10 + Number(digits[1]);
+};
+
+const getDigits = (line, digits = []) => {
   line = line.toString();
 
-  let left = -1;
-  let right = line.length;
-
-  while (left + 1 < line.length && !Number(line[left + 1])) left++;
-  while (right - 1 > -1 && !Number(line[right - 1])) right--;
-
-  left++;
-  right--;
-
-  if (
-    left === -1 ||
-    left >= line.length ||
-    right === line.length ||
-    right < 0
-  ) {
-    result += 0;
-  } else {
-    result += Number(line[left]) * 10;
-    result += Number(line[right]);
+  if (line.length === 0) {
+    return digits;
   }
 
-  return result;
+  if (Number(line[0]) && digits.length === 0) {
+    digits = [line[0], line[0]];
+  } else if (Number(line[0])) {
+    digits = [digits[0], line[0]];
+  }
+
+  return getDigits(line.slice(1), digits);
 };
 
 const lines = argv._;
