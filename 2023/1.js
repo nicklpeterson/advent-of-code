df24b16a34006e726607e3bddc099227fdc4ba20
nick.peterson <nick.peterson@target.com>
2023-12-01 17:57:56 -0600

    day 1

--- 2023/1.js
+++ 2023/1.js
@@ -5,17 +5,17 @@ const solve = (lines) =>
 
 const getValueOfLine = (line) => {
   const digits = getDigits(line);
+
   return Number(digits[0]) * 10 + Number(digits[1]);
 };
 
 const getDigits = (line) =>
   Array.from(line.toString()).reduce((digits, char) => {
-    if (Number(char) && digits.length === 0) {
-      digits = [char, char];
-    } else if (Number(char)) {
-      digits = [digits[0], char];
+    if (!Number(char)) {
+      return digits;
     }
-    return digits;
+
+    return digits.length === 0 ? [char, char] : [digits[0], char];
   }, []);
 
 const lines = argv._;
