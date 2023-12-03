11da0d661a1a8773a73bf25875d1b2109065b2b7
nick.peterson <nick.peterson@target.com>
2023-12-03 16:23:40 -0600

    vscode snippet template

---
+++ 2023/4.js
@@ -0,0 +1,12 @@
+const argv = require('minimist')(process.argv.slice(2));
+
+const parseArgv = (input) => {}
+
+const solvePart1 = (input) => {}
+
+const solvePart2 = (input) => {}
+
+const input = parseArgv(argv._)
+
+console.log('Part 1 - ', solvePart1(input));
+console.log('Part 2 - ', solvePart2(input));
