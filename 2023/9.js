53411a4004c8c35eaed701e5bd0d3ce2cee2ffcf
nick.peterson <nick.peterson@target.com>
2023-12-09 12:27:54 -0600

    day 9

--- 2023/9.js
+++ 2023/9.js
@@ -1,29 +1,27 @@
-const dydx = (data, layers = [data]) => {
-  if (data.every((y) => y === 0)) return layers;
+const extrapolate = (data, layers = [data]) => {
+  if (data.every((y) => y === 0)) return calculateValues(layers);
   const layer = [];
   for (let i = 1; i < data.length; i++) layer.push(data[i] - data[i - 1]);
-  return dydx(layer, [...layers, layer]);
+  return extrapolate(layer, [...layers, layer]);
 };
 
-const extrapolate = (layers) => {
+const calculateValues = (layers) => {
   if (layers.length === 1) return layers[0];
 
   const bottomLayer = layers.pop();
-  const bottomLast = bottomLayer.pop();
-  const bottomFirst = bottomLayer.shift();
 
-  const currentLayer = layers.pop();
-  const currentLast = currentLayer.pop();
-  const currentFirst = currentLayer.shift();
+  layers[layers.length - 1] = [
+    layers.at(-1).shift() - bottomLayer.shift(),
+    layers.at(-1).pop() + bottomLayer.pop(),
+  ];
 
-  layers.push([currentFirst - bottomFirst, currentLast + bottomLast]);
-  return extrapolate(layers);
+  return calculateValues(layers);
 };
 
 const solve = (input) => {
   const result = { p1: 0, p2: 0 };
   while (input.length) {
-    const [prev, next] = extrapolate(dydx(input.splice(0, 21)));
+    const [prev, next] = extrapolate(input.splice(0, 21));
     (result.p1 += next), (result.p2 += prev);
   }
   return result;
